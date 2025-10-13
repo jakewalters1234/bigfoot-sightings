6278a0a44cbe7b9c9f4cb33fac22166d782d7887
Jake Walters <jwalters@mail.wlu.edu>
2025-10-13 15:44:31 -0400

    New data table for wordcloud

--- server.R
+++ server.R
@@ -1,12 +1,14 @@
-bigfoot_data <- read.csv("bigfoot_data.csv")
+bigfoot_data <- read.csv("bigfoot_data_wordcount_filtered_less_200.csv")
 
 library(wordcloud)
 library(tm)
 library(memoise)
+library(tidyverse)
+library(stringr)
 
 function(input, output) {
   output$WordCloud <- renderPlot({
-    wordcloud(bigfoot_data$state)
+    wordcloud(bigfoot_data$observed)
   })
 }
 
